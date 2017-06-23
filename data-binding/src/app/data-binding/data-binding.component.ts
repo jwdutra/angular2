6f64390bf425c07f1f36130294a9f58a3bc45013
José Walter <jwdutra@gmail.com>
2017-06-22 21:08:05 -0300

    Estudos do data-binding

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url:string = 'http://www.jwdutra.com.br';
+  cursoAngular: boolean = true;
+  urlImagem = 'http://lorempixel.com/400/200/';
+
+  valorAtual: string = '';
+  valorSalvo;
+
+  isMouseOver: boolean = false;
+
+  nomeDoCurso: string = "Curso Angular";
+
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  botaoClicado(){
+    alert('Clicado');
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+    onMouseOverOut(){
+        this.isMouseOver = !this.isMouseOver;
+    }
+
+  onKeyUp(evento: KeyboardEvent){
+    //console.log((<HTMLInputElement>evento.target).value);
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
