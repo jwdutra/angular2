82b56c63b49b3b46fd509be21c311ee753263ae9
José Walter <jwdutra@gmail.com>
2017-06-25 17:51:21 -0300

    Exemplo de direticva customizada com Renderer

--- diretivas/src/app/shared/fundo-amarelo.directive.ts
+++ diretivas/src/app/shared/fundo-amarelo.directive.ts
@@ -1,16 +1,19 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Renderer } from '@angular/core';
 
 @Directive({
   selector: '[fundoAmarelo]'
 })
 export class FundoAmareloDirective {
 
-  // Para aplicar somente a um tipo de elemento coloca-se no selector ex: p[fundoAmarelo], aplicar só a parágrafos 
-  // 
+  // Para aplicar somente a um tipo de elemento coloca-se no selector ex: p[fundoAmarelo], aplicar só a parágrafos (não esquecer de fazer o import)
 
-  constructor(private _elementRef: ElementRef) { 
+  // Por boas práticas, é recomendado não suar o ElementRef por questões de segurança
+  // No lugar dele usar o Renderer
+
+  constructor(private _elementRef: ElementRef, private _renderer: Renderer) { 
     console.log(this._elementRef);
-    this._elementRef.nativeElement.style.backgroundColor = 'yellow';
+    //this._elementRef.nativeElement.style.backgroundColor = 'yellow'; // Exemplo com ElementRef
+    this._renderer.setElementStyle(this._elementRef.nativeElement,'background-color','yellow'); // exemplo com Renderer
   }
 
 }
