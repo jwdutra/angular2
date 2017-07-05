b9f25bfc48f9bc1add97df8d7b39a0977a9ea3fb
José Walter <jwdutra@gmail.com>
2017-07-05 16:54:12 -0300

    Duas instâncias do serviço, uma global e outra local

--- servicos/src/app/cursos/cursos.component.ts
+++ servicos/src/app/cursos/cursos.component.ts
@@ -5,7 +5,8 @@ import { CursosService } from './cursos.service';
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+  styleUrls: ['./cursos.component.css'],
+  providers: [CursosService] /// Se comentarmos cria apenas uma instancia
 })
 export class CursosComponent implements OnInit {
 
--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -7,12 +7,17 @@ import{ Injectable } from '@angular/core';
 @Injectable()  
 export class CursosService {
 
+    private cursos: string[] = ['Angular','PHP','Java'];;
     constructor() {
         console.log('CursosService');
     }
 
     getCursos() {
-        return ['Angular','PHP','Java'];
+        return this.cursos;
+    }
+
+    addCurso(curso: string) {
+        this.cursos.push(curso);
     }
 
 }
